6658d5ab7673b1634cb0068eba45a389842f73eb
BertCasale <bthebest55@gmail.com>
2023-06-20 21:31:08 -0400

    timePasses method added

--- tamagotchi.js
+++ tamagotchi.js
@@ -53,6 +53,24 @@ class Tamagotchi {
     }
   }
 
+  sleep(){
+    this.energy += 4;
+    this.full -= 3;
+  }
+
+  timePasses(){
+    if (!this.sick){
+      this.mood -= 2;
+      this.full--;
+      this.energy--;
+    } else {
+      this.mood -= 3;
+      this.full -= 2;
+      this.energy -= 2;
+    }
+  }
+
+  
 
 }
 
