8387a390cfb8b3b009de95000e72bd1814705c65
ninajavier <ninajavier13@gmail.com>
2023-06-20 20:57:35 -0400

    created Food and Tamagotchi classes, added prepare method

--- food.js
+++ food.js
@@ -1,4 +1,16 @@
 // Create class below
+class Food {
+    constructor(name, daysToSpoil, fresh) {
+        this. name = name;
+        this.daysToSpoil = daysToSpoil || 0;
+        this.fresh = fresh || true;
+    }
+    prepare(){
+        console.log(`${this.name} is being prepared`);
+    }
+    
+}
+
 
 // Do not edit below this line
 module.exports = Food;
--- tamagotchi.js
+++ tamagotchi.js
@@ -1,4 +1,12 @@
 // Create class below
-
+class Tamagotchi {
+    constructor (name, energy, full, mood, sick, rehomed) {
+        this. name = name;
+        this.energy = energy || 9;
+        this.full = full || 8;
+        this.sick = sick || false;
+        this.rehomed = rehomed || false;
+    }
+}
 // Do not edit below this line
 module.exports = Tamagotchi;
