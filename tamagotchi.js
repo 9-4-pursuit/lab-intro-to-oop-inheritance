6f256f17971f94d7a668b794ddeda6d0e1d315d5
aardvarkpepper <aardvarkpepper@gmail.com>
2023-06-20 20:42:49 -0400

    add Tamagotchi methods 'eat' and 'medicate'.

--- tamagotchi.js
+++ tamagotchi.js
@@ -15,6 +15,25 @@ class Tamagotchi {
         const star = '*';
         console.log(`${star.repeat(20)}\nMy mood is: ${this.mood}\nI am this full: ${this.full}\nMy energy is: ${this.energy}\nI am ${this.sick ? "sick" : "not sick"}\n${star.repeat(20)}`)
     }
+    eat() {
+        this.full += 2;
+        this.energy -= 1;
+        if (this.full > 10) {
+            this.sick = true;
+        }
+    }
+    medicate() {
+        if (this.sick = true) {
+            this.full = 9;
+            this.energy -= 3;
+        } else {
+            console.log(`${this.name} does not think it needs medicine!`);
+            this.energy -= 1;
+        }
+    }
 }
+const hamzilla = new Tamagotchi("Hamzilla");
+hamzilla.greet();
+hamzilla.status();
 // Do not edit below this line
 module.exports = Tamagotchi;
