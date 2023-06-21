80b3e0402fbe08a0bbda8bdc81117a7f9d0e519d
Diegocarrero <diegocar2413@gmail.com>
2023-06-20 21:27:12 -0400

    add status method and testing

--- tamagotchi.js
+++ tamagotchi.js
@@ -14,8 +14,15 @@ class Tamagotchi {
         console.log(`Hello, I'm ${this.name}!`);
     }
 
-    
+    status() {
+        console.log(`My mood is: ${this.mood}\nI am this full: ${this.full}\nMy energy is: ${this.energy}`);
+        this.sick ? console.log('I am sick') : console.log('I am not sick');
+    }
 }
+
+const hercules = new Tamagotchi('Hercules');
+
+hercules.status()
 
 // Do not edit below this line
 module.exports = Tamagotchi;
