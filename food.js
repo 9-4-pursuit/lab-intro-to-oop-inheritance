fb6ab4ba5c3e677008c97d91bdb0f61bb00c9aa9
BertCasale <bthebest55@gmail.com>
2023-06-20 20:27:54 -0400

    food methods finished

--- food.js
+++ food.js
@@ -7,18 +7,24 @@ class Food {
   }
 
   prepare(){
-
+    console.log(`${this.name} is being prepared`);
   }
 
   isFresh(){
+    if (this.daysToSpoil <= 0){
+      this.fresh = false;
+    }
 
+    this.fresh ? 
+    console.log(`There are ${this.daysToSpoil} days left before ${this.name} spoils.`) :
+    console.log(`${this.name} has spoiled.`);
   }
 
   aDayPasses(){
-    
+    this.daysToSpoil--;
+    this.isFresh()
   }
 }
-
 
 // Do not edit below this line
 module.exports = Food;
