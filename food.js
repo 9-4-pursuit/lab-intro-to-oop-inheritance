6aca3c1584ea74ebbf14d5daecb1456294825ff6
Mark Robertson <markrobertson67@gmail.com>
2023-06-20 20:31:46 -0400

    initial commit

--- food.js
+++ food.js
@@ -1,4 +1,21 @@
 // Create class below
 
+class Food {
+    constructor(name, daysToSpoil, fresh = true) {
+        this.name = name;
+        this.daysToSpoil = daysToSpoil;
+        this.fresh = fresh
+    }
+
+    prepare() {
+        console.log(`${this.name} is being prepared`)
+    }
+}
+
+
+
+
+
+
 // Do not edit below this line
 module.exports = Food;
