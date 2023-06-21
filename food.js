743ccfafe231902cd7d99916225956a708b755c0
SuperNinjaEv <evan0210@gmail.com>
2023-06-20 22:00:13 -0400

    All food tests passed

--- food.js
+++ food.js
@@ -1,4 +1,39 @@
 // Create class below
 
+class Food {
+    constructor(name, daysToSpoil, fresh){
+        this.name = name || "Default food";
+        this.daysToSpoil = daysToSpoil || 10;
+        this.fresh = fresh || daysToSpoil > 0 ? true : false;
+    }
+
+    prepare(){
+        console.log(`${this.name} is being prepared`);
+    }
+
+    isFresh(){
+        if(this.fresh){
+            console.log(`There are ${this.daysToSpoil} days left before ${this.name} spoils.`)
+        } else {
+            console.log(`${this.name} has spoiled.`)
+        };
+    }
+
+    aDayPasses(){
+        this.daysToSpoil--;
+        if(this.daysToSpoil < 1){
+            this.fresh = !this.fresh;
+        };
+        console.log(this.daysToSpoil)
+        this.isFresh();
+    }
+}
+
+const tacos = new Food("Tacos", 1, true)
+// console.log(tacos);
+tacos.prepare();
+tacos.isFresh();
+tacos.aDayPasses();
+
 // Do not edit below this line
 module.exports = Food;
--- tamagotchi.js
+++ tamagotchi.js
@@ -1,4 +1,63 @@
 // Create class below
 
+class Tamagotchi {
+    constructor(name, energy, full, mood, sick, rehomed) {
+        this.name = name;
+        this.energy = energy || 9;
+        this.full = full || 8;
+        this.mood = mood || 6;
+        this.sick = sick || false;
+        this.rehomed = rehomed || false;
+    }
+
+    greet() {
+        console.log(`Hello, I'm ${this.name}`);
+    }
+
+    status() {
+        console.log(`******************** \n
+        My mood is: ${this.mood}\n
+        I am this full: ${this.full}\n
+        My energy is: ${this.energy}\n
+        I am not sick`);
+    }
+
+    eat() {
+        this.full += 2;
+        this.energy--;
+        console.log(this.full, this.energy);
+
+        this.full > 10 ? this.sick = true : null;
+    }
+
+    medicate() {
+        if (this.sick) {
+            this.full = 9;
+            this.energy -= 3;
+            this.sick = false;
+        } else {
+            this.energy--;
+            console.log(`${this.name} won't take medicine.`)
+        };
+    }
+
+    play() {
+        if (!this.sick && this.mood <= 9 && this.energy > 3) {
+            this.mood += 2;
+            this.energy--;
+            this.full--;
+        } else if (this.sick) {
+            this.mood--;
+            this.energy--;
+        } else if (this.mood > 9) {
+            this.energy -= 2;
+            this.full--;
+        } else if (this.energy <= 3){
+            this.energy--;
+            console.log("I am too tired to play.")
+        }
+    }
+}
+
 // Do not edit below this line
 module.exports = Tamagotchi;
