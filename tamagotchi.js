5398e71e9d9d33c5b84d9aa0b3db7f5e9dec9c4b
keriarmstrong <khetto@live.com>
2023-06-20 21:30:47 -0400

    completed the food lab, all test for that section passed

--- tamagotchi.js
+++ tamagotchi.js
@@ -1,4 +1,18 @@
 // Create class below
 
+
+class Tamagotchi {
+    constructor(name, energy, full, mood, sick, rehomed){
+        this.name = name 
+        this.energy = energy 
+        this.full = full 
+        this.mood = mood 
+        this.sick = sick 
+        this.rehomed = rehomed
+    }
+
+}
+
+
 // Do not edit below this line
 module.exports = Tamagotchi;
