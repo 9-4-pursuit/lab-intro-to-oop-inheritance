e418648e027fa4303a077bae74be942a9ede673b
Mark Robertson <markrobertson67@gmail.com>
2023-06-20 21:26:51 -0400

    food finished

--- food.js
+++ food.js
@@ -10,11 +10,36 @@ class Food {
     prepare() {
         console.log(`${this.name} is being prepared`)
     }
-}
 
 
+    isFresh() {
+        if (this.daysToSpoil > 1) {
+            console.log(`There are ${this.daysToSpoil} days left before ${this.name} spoils.`);
+        } else if (this.daysToSpoil === 1) {
+            console.log(`There is 1 day left before ${this.name} spoils.`);
+        } else if (this.daysToSpoil === 0) {
+            console.log(`${this.name} has spoiled.`);
+        }
+    }
 
 
+    aDayPasses() {
+        this.daysToSpoil -= 1;
+        this.isFresh();
+      }
+    
+      simulateSpoiling() {
+        while (this.daysToSpoil >= 0) {
+          this.aDayPasses();
+        }
+      }
+    }
+    
+    const icecream = new Food("icecream", 3, true);
+    
+    icecream.prepare();
+    icecream.isFresh();
+    icecream.simulateSpoiling(); 
 
 
 // Do not edit below this line
