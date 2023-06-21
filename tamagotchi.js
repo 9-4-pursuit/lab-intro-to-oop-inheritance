139ea3be382c12dd712809185a04a41176945048
Wilsantos1975 <Faustosantos@pursuit.org>
2023-06-20 22:15:30 -0400

    created greet and status methods

--- tamagotchi.js
+++ tamagotchi.js
@@ -1,5 +1,22 @@
 // Create class below
-class tama
+class Tamagotchi {
+    construnctor(name, energy, full, mood,) {
+        this.name = name;
+        this.energy = energy;
+        this.full = full;
+        this.mood = mood;
+        this.sick = false;
+        this.rehomed = false;
+    }
+        greet(){
+            console.log(`Hello, I'm ${this.name}`);
+
+        }
+        status(){
+        }
+        
+    }
+
 
 // Do not edit below this line
 module.exports = Tamagotchi;
