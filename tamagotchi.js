4b413a6f622cdebbcce7f2e85b51be6011375167
Diegocarrero <diegocar2413@gmail.com>
2023-06-20 21:34:51 -0400

    add medicate method

--- tamagotchi.js
+++ tamagotchi.js
@@ -24,12 +24,24 @@ class Tamagotchi {
         this.full += 2;
         this.full > 10 ? this.sick = true : null;
     }
+
+    medicate() {
+        if (this.sick) {
+            this.full = 9;
+            this.energy -= 3;
+        } else {
+            console.log('refusal to take medicine');
+            this.energy--;
+        }
+    }
 }
 
 const hercules = new Tamagotchi('Hercules');
 
 hercules.greet();
 hercules.status();
+hercules.eat();
+hercules.medicate();
 
 // Do not edit below this line
 module.exports = Tamagotchi;
