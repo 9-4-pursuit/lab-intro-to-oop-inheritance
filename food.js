3dd5615cc4e549969ac88413038636b91c585484
BertCasale <bthebest55@gmail.com>
2023-06-20 20:08:00 -0400

    create food class and constructor

--- food.js
+++ food.js
@@ -1,4 +1,13 @@
 // Create class below
+class Food {
+  constructor(name, daysToSpoil = 0, fresh = true){
+    this.name = name;
+    this.daysToSpoil = daysToSpoil;
+    this.fresh = fresh;
+  }
+  
+}
+
 
 // Do not edit below this line
 module.exports = Food;
