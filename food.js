413afee453c9cdf30e5b7f5a7028c65934b4b9c3
BertCasale <bthebest55@gmail.com>
2023-06-20 21:51:55 -0400

    bonus prepare method added

--- food.js
+++ food.js
@@ -27,13 +27,40 @@ class Food {
 }
 
 class BadFood extends Food{
-  constructor(name, daysToSpoil = 20, fresh = true, weapons){
+  constructor(name, weapons, daysToSpoil = 20, fresh = true){
     super(name, daysToSpoil);
     this.fresh = fresh; 
     this.weapons = weapons;
   }
 
-  
+  prepare(){
+    let taunts = [
+      `I am ${this.name} and my calories are too high to count!`,
+      `I am ${this.name} and you are just a passing trend!`,
+      `You're about to get burned!`
+    ]
+
+    console.log(taunts[Math.floor(Math.random() * taunts.length)])
+  }
+
+
+
 }
+
+attacks = [
+  {
+    name: "Mouth Burn",
+    hitPoints: 3,
+  },
+  {
+    name: "Hot Pepper Storm",
+    hitPoints: 4,
+  },
+  {
+    name: "Cheese Grease",
+    hitPoints: 5,
+  },
+];
+
 // Do not edit below this line
 module.exports = Food;
