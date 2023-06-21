719fa5e484a8b6697f06ddf1d4ebfbbfbdcce13f
pjungjs <pjungjs@gmail.com>
2023-06-20 23:38:00 -0400

    update on Food class freshness

--- food.js
+++ food.js
@@ -3,7 +3,7 @@ class Food {
   constructor(name, daysToSpoil, fresh = true) {
     this.name = name;
     this.daysToSpoil = daysToSpoil;
-    this.fresh = fresh;
+    this.fresh = daysToSpoil > 0 ? true : false;
   }
 
   prepare() {
@@ -11,17 +11,22 @@ class Food {
   }
 
   isFresh() {
-    if (this.daysToSpoil > 1) {
+    if (this.daysToSpoil > 1 && this.fresh) {
       console.log(`There are ${this.daysToSpoil} days left before ${this.name} spoils.`);
-    } else if (this.daysToSpoil === 1) {
-      console.log(`There is ${this.daysToSpoil} day left before ${this.name} spoils.`)
+    } else if (this.daysToSpoil === 1 && this.fresh) {
+      console.log(`There is ${this.daysToSpoil} day left before ${this.name} spoils.`);
     } else {
-      console.log(`${this.name} has spoiled.`)
+      console.log(`${this.name} has spoiled.`);
     }
+  }
+
+  updateFreshnessToFalse() {
+    this.fresh = false;
   }
 
   aDayPasses() {
     this.daysToSpoil--;
+    if (this.daysToSpoil <= 0) this.updateFreshnessToFalse();
     this.isFresh();
   }
 }
