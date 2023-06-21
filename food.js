afddf49cb769b52468dce14741d025fed2dda8e8
aardvarkpepper <aardvarkpepper@gmail.com>
2023-06-20 20:29:13 -0400

    food.js methods and test OK.

--- food.js
+++ food.js
@@ -1,4 +1,31 @@
 // Create class below
+class Food {
+    constructor (name, daysToSpoil, fresh = true) {
+        this.name = name;
+        this.daysToSpoil = daysToSpoil;
+        this.fresh = fresh;
+    }
+    prepare() {
+        console.log(`${this.name} is being prepared`);
+    }
+    isFresh() {
+        if (this.daysToSpoil > 0) {
+            console.log(`There are ${this.daysToSpoil} days left before ${this.name} spoils`);
+        } else {
+            this.fresh = false;
+            console.log(`${this.name} has spoiled.`)
+        }
+    }
+    aDayPasses() {
+        this.daysToSpoil--;
+        this.isFresh();
+    }
+}
+
+const hamsterPellet = new Food ("Hamster Pellet", 1, true);
+hamsterPellet.prepare();
+hamsterPellet.isFresh();
+hamsterPellet.aDayPasses();
 
 // Do not edit below this line
 module.exports = Food;
