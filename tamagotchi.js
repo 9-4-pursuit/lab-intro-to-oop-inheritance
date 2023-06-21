eb5c3ce55d8f1df95ded5df693188b344e87c68f
BertCasale <bthebest55@gmail.com>
2023-06-20 20:37:03 -0400

    tamagotchi constructor made

--- tamagotchi.js
+++ tamagotchi.js
@@ -1,6 +1,18 @@
 // Create class below
 class Tamagotchi {
-  
+  constructor(name, energy = 9, full = 8, mood = 6, sick = false, rehomed = false){
+    this.name = name;
+    this.energy = energy;
+    this.full = full;
+    this.mood = mood;
+    this.sick = sick;
+    this.rehomed = rehomed;
+  }
+
 }
+
+let tomo = new Tamagotchi("tomo");
+
+console.log(tomo)
 // Do not edit below this line
 module.exports = Tamagotchi;
