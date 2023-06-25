39fc78d60cec754fd6e3ee17a73e5a90c57a0957
SamuelBAlba <brayanbalba24@gmail.com>
2023-06-25 09:06:10 -0400

    tama

--- tamagotchi.js
+++ tamagotchi.js
@@ -8,9 +8,67 @@ class Tamagotchi {
         this.sick = sick;
         this.rehomed = rehomed;
     }
+
+    greet() {
+        console.log(`Hello, I'm ${this.name}`)
+    }
+
+    status() {
+        let sicky = ""
+
+        if(this.sick === false){
+            sicky = 'I am not sick'
+        }else {
+            sicky = 'I am sick'
+        }
+
+        console.log(`My mood is: ${this.mood}`)
+        console.log(`I am this full: ${this.full}`)
+        console.log(`My energy is: ${this.energy}`)
+        console.log(sicky)
+    }
     
+    eat() {
+        this.full += 2
+        this.energy--
+
+        if(this.full > 10){
+            this.sick = true
+        }
+    }
+
+    medicate() {
+        if(this.sick === true) {
+            this.full = 9
+            this.energy -= 3
+        }else {
+            console.log('I dont want medicine')
+            this.energy--
+        }
+    }
+
+    sleep() {
+        this.energy += 4
+        this.full -= 3
+
+    }
+    
+    timePases() {
+        if(this.sick === true) {
+            this.mood -= 3
+            this.full -= 2
+            this.energy -= 2
+        }else {
+            this.mood -= 2
+            this.full -= 2
+            this.energy -= 2
+        }
+    }
 }
 
+const pika = new Tamagotchi ('Pikachu')
+console.log(pika.timePases())
+console.log(pika)
 
-// Do not edit below this line
-module.exports = Tamagotchi;
+
+
