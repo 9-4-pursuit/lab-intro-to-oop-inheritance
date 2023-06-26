a46f36e6d8c7b863e7cb249fe4b0089a7bafc2c9
Wilsantos1975 <Faustosantos@pursuit.org>
2023-06-26 17:42:06 -0400

    food class completed

--- food.js
+++ food.js
@@ -37,40 +37,5 @@ iceCream.isFresh(); // Output: There are 3 days left before Ice Cream spoils.
 // Call the aDayPasses method
 iceCream.aDayPasses(); // Output: There are 2 days left before Ice Cream spoils.
 
-  
 
-// class Food {
-//     constructor(name, daysToSpoil) {
-//         this.name = name;
-//         this.daysToSpoil = daysToSpoil;
-//         this.isFresh = true;
-//     }
-//     prepare() {
-//         console.log(`${this.name} is being prepared`);
-//     }
-
-
-//     isFresh() {
-
-//         // if (this.daysToSpoil > 0) {
-//         //     console.log(`There are ${this.daysToSpoil} days left before ${this.name} spoils`);
-//         // } else {
-//         //     console.log(`${this.name} has spoiled`);
-//         // }
-//         this.daysToSpoil > 0 ? console.log(`There are 3 days left before ${this.name} spoils`) : console.log(`${this.name} has spoiled `);
-        
-//     }
-
-//     aDayPasses() {
-        
-//         this.daysToSpoil -= 1;
-//         this.isFresh();
-//     }   
-
-// }
-
-
-// const food = new Food('apple', 3, true);
-// console.log(food.isFresh())
-// Do not edit below this line
 module.exports = Food;
