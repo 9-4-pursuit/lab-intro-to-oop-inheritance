b0a687c1d71822513a4bf3d66bb77b05c2b3b3a0
jRodriguezIV <j.a.rodrigueziv@gmail.com>
2023-07-16 13:49:31 -0400

    wrote Food class , and the methods : prepare, isFresh, aDayPasses.

--- food.js
+++ food.js
@@ -1,4 +1,30 @@
 // Create class below
 
+class Food { 
+    constructor(name, daysToSpoil, fresh = true){
+        this.name = name;
+        this.daysToSpoil = daysToSpoil;
+        this.fresh = fresh;
+    }
+
+    prepare() {
+        return console.log(`${this.name} is being prepared`);
+    }
+
+    isFresh() {
+        if (this.fresh) {
+            return console.log(`There are ${this.daysToSpoil} left before ${this.name} spoils.`)
+        } else {
+            return console.log(`${this.daysToSpoil} or less days left, Item has spoiled.`)
+        }
+
+        
+    }
+
+    aDayPasses() {
+        this.daysToSpoil -= 1;
+    }
+}
+
 // Do not edit below this line
 module.exports = Food;
