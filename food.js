6826bfba87d58b5a788b39225c27888f218a645b
MichelBDLC <michelbdlc@gmail.com>
2023-07-17 00:48:12 -0700

    food class has two of same test, one passes and one fails; error in rest code.

--- food.js
+++ food.js
@@ -1,4 +1,32 @@
 // Create class below
 
+class Food {
+    constructor(name, daysToSpoil, fresh = true) {
+        this.name = name; //name || ''
+        this.daysToSpoil = daysToSpoil; //might need to fix, looks uncalled
+        this.fresh = fresh;
+    }
+
+    prepare() {
+        console.log(`${this.name} is being prepared`);
+    }
+
+    isFresh() {
+        if (this.isFresh = true) {
+            console.log(`There are ${this.daysToSpoil} days left before ${this.name} spoils.`)
+        }
+
+        else {
+            console.log(`${this.name} has spoiled.`)
+        }
+    }
+
+    aDayPasses() {
+        this.daysToSpoil - 1; // --
+
+        this.isFresh(); //might need to incorporate this. if no work 
+    }
+}
+
 // Do not edit below this line
 module.exports = Food;
